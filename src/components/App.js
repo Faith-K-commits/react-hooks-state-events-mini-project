a75aed181f59cb9b274cb47f1451e62cb767d94a
Faith Kariuki <tcwamboi@gmail.com>
2024-08-05 17:55:14 +0300

    First Commit

--- src/components/App.js
+++ src/components/App.js
@@ -3,17 +3,19 @@ import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
 
+
 import { CATEGORIES, TASKS } from "../data";
 console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
 
 function App() {
+  
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
+      <CategoryFilter categories={CATEGORIES} />
       <NewTaskForm />
-      <TaskList />
+      <TaskList Tasks={TASKS} />
     </div>
   );
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList({ Tasks }) {
+  const [taskLists, setTaskLists] = useState([]);
+
+  const handleDelete = (taskId) => {
+    const updatedTasks = taskLists.filter(taskList => taskList.text !== taskId);
+    console.log("Deleting task of id:", taskId);
+    setTaskLists(updatedTasks);
+
+  };  
   return (
     <div className="tasks">
       {/* display a list of tasks using Task component */}
+      <ul>
+        {Tasks.map((task) => (
+          <Task key={task.text} text={task.text} category={task.category} onDelete={handleDelete} />
+        ))}
+      </ul>
     </div>
   );
 }
